edb1856d2def7b02ecc00939c57f6984cd1f77c3
Hemalatha-SITA <Hemalatha@stockholmitacademy.org>
2024-03-03 22:00:34 +0100

    file added

---
+++ Array Syntax/syntax.js
@@ -0,0 +1,13 @@
+// Find largest number
+
+function findLargestNumber() {
+  return Math.max(...arr); //( arr- arrayName)
+}
+
+// Find smallest number
+
+function findSmallestNumber() {
+  return Math.min(...num); //( num- arrayName)
+}
+
+
